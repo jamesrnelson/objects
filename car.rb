020814c9a3d2a139eefb58dad865cf1be5615676
JNelson <nelson.jimmy@gmail.com>
2018-01-23 19:21:29 -0700

    Figured out my issue with the color method had to do with missing punctuation in the attr_accessor.

--- car.rb
+++ car.rb
@@ -1,27 +1,35 @@
 class Car
-  attr_accessor :color
+  attr_accessor :horn,
+                :distance,
+                :color,
+                :wheel_count
 
-  def initialize(color)
+  def initialize(horn, distance, color, wheel_count)
+    @horn = horn
+    @distance = distance
     @color = color
-  end
-
-  def horn
-    puts "BEEEEEP!"
+    @wheel_count = wheel_count
   end
 
   def drive(distance)
     puts "I'm driving #{distance} miles."
   end
 
-  def report_color(color)
-    puts "I am #{color}."
+  def report_color
+    puts "I am #{@color}."
   end
 
-
+  def sweet_ride
+    puts "This sweet ride is sitting on #{@wheel_count} wheels!"
+  end
 end
 
-my_car = Car.new
+my_car = Car.new("BEEP BEEP!", 12, "purple", 18)
 puts my_car.horn
+puts my_car.distance
 puts my_car.drive(12)
-my_car.color = 'purple'
 puts my_car.report_color
+puts my_car.sweet_ride
+
+my_second_car = Car.new("BEEEEEEEP", 20, "green", 2)
+puts my_second_car.sweet_ride
